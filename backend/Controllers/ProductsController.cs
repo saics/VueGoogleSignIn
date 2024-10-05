85e5d3385153594e4a4231924f5070f4e64c9de6
saics <simunsaic01@gmail.com>
2024-10-05 20:04:17 +0200

    Add Backend

---
+++ backend/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using backend.Data;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ProductsController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly ILogger _logger;
+
+        public ProductsController(AppDbContext context, ILogger<ProductsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
